c041f784884e6e3f3add257977549808e70efe21
DYuHLin <damianver2000@gmail.com>
2022-10-24 11:41:45 +0200

    made the grid size button able to set the size of the grid

--- main.js
+++ main.js
@@ -12,19 +12,34 @@ const grid = document.getElementById("grid");
 const defaultColor = "#333333";
 const defaultSize = 16;
 
+let cells;
+
 //function to make grid
 function makeGrid(size){
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
     for(i = 0; i < (size * size); i++){
-        let cells = document.createElement("div");
+        cells = document.createElement("div");
         cells.classList.add("ind__grid");
-        grid.appendChild(cells)
-    }
+        cells.classList.add("grid__line");
+        grid.appendChild(cells);
+    };
 };
 
+//this button action allows you to set the size of the grid
+sizeBtn.addEventListener('click', (e) => {
+    let gridSize = parseInt(prompt("Enter a value between 1 and 100"));
 
+    if(gridSize > 100){
+        alert("You must enter a value between 1 and 100");
+    } else if (gridSize < 1){
+        alert("You must enter a value between 1 and 100");
+    } else {
+        grid.innerHTML = "";
+        makeGrid(gridSize);
+    }
+});
 
 window.onload = () => {
-    makeGrid(defaultSize)
-}
+    makeGrid(defaultSize);
+};
